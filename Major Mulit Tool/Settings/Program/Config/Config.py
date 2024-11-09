ba460102fe69d59f2c394cc511771faa0e050a71
Major Tools <saratalab61@gmail.com>
2024-11-09 10:46:34 +0200

    Major Tool
    By Reo - Mr3b

---
+++ Major Mulit Tool/Settings/Program/Config/Config.py
@@ -0,0 +1,18 @@
+# Copyright (c) MajorTool2s  (discord.gg/YRT6SZ8Z)
+# السلام عليكم يا جماعه دي حقوقنا فا يريت محدش يغير حاجه تمام ؟ 
+
+name_tool       =  "MajorTool2s"
+type_tool       =  "Multi-Tools"
+version_tool    =  "6.1"
+coding_tool     =  "Python 3"
+language_tool   =  "EN"
+creator         =  "Reo-Mr3b"
+platform        =  "Windows 10/11 & Linux"
+discord_server  =  "None"
+website         =  "discord.gg/YRT6SZ8Z"
+github_tool     =  "https://github.com/1990Group"
+telegram        =  "t.me/"
+url_config      =  "https://github.com/1990Group"
+url_downloads   =  "https://github.com/1990Group"
+license         =  "https://github.com/1990Group"
+copyright       =  "Copyright (c) MajorTool2s 'GPL-3.0 license'"
